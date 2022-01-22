9219778ce97a1a13cb5704982e4b33a6bb6233ad
William Bolton <william.ellet@gmail.com>
2022-01-22 11:39:40 -0500

    Adds first prompt for config checking to init command

--- internal/initializer/initializer.go
+++ internal/initializer/initializer.go
@@ -1,7 +1,34 @@
 package initializer
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+	"github.com/webolton/skink/internal/lib"
+)
 
 func Execute() {
-	fmt.Print("INIT IS CALLED")
+	newConfigPrompt := promptui.Select{
+		Label: "Would you like to create a new skink configuration?",
+		Items: []string{"yes", "no"},
+	}
+
+	_, result, err := newConfigPrompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	if result == "yes" {
+		if lib.CheckFile("~/.skink.conf") {
+			fmt.Println("there is a file")
+		} else {
+			fmt.Println("no file")
+		}
+	}
+
+	if result == "no" {
+		fmt.Println("old config")
+	}
 }
