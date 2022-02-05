7b36542bcdcbfe529ad37f0c7d8f5d550c0d51d6
William Bolton <william.ellet@gmail.com>
2022-02-05 12:26:02 -0500

    Improve init message

--- internal/commands/init.go
+++ internal/commands/init.go
@@ -5,11 +5,6 @@ import (
 	"github.com/webolton/skink/internal/initializer"
 )
 
-type promptContent struct {
-	errorMsg string
-	label    string
-}
-
 func init() {
 	rootCmd.AddCommand(initializeCmd)
 }
@@ -17,8 +12,7 @@ func init() {
 var initializeCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Setup skink",
-	Long: `Provide the directory for skink to watch, the cloud storage and key where
-skink is to push your files, and a location for this configuration.`,
+	Long:  `Setup and configure skink.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		initializer.Execute()
 	},
